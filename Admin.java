c4a60fd84f39873b05f64fff13cfdc856ee931f4
jsanchez-codes <julian.sanchez@bsu.edu>
2026-04-23 20:02:36 +0000

    Created Admin class

---
+++ Admin.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Admin extends User implements HelpDesk, Serializable {
+
+	public static void main(String[] args){
+		Admin a = new Admin();
+		a.login();
+	} // end main
+	
+
+	public Admin(){
+		this.userName = "admin";
+		this.password = "password";
+	} // end constructor
+	
+	public String menu(){
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Admin Menu");
+		System.out.println();
+		System.out.println("0) Sign Out");
+		System.out.println("1) Add new user");
+		System.out.println("2) Delete user");
+		System.out.println();
+		System.out.print("Action (0-2): ");
+
+		String response = input.nextLine();
+		return response;
+	} // end menu
+	
+} // end Admin class
