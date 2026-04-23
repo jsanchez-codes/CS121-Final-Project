52b29c197ac2eb5f0603d2ba3af2705c37d796a1
jsanchez-codes <julian.sanchez@bsu.edu>
2026-04-23 21:44:51 +0000

    Created TicketManager Class

---
+++ TicketManager.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class TicketManager implements HelpDesk {
+
+	Admin admin = new Admin();
+	TicketList tickets = new TicketList();
+
+	public static void main(String[] args){
+		new TicketManager();
+	} // end main
+	
+	public TicketManager(){
+		this.loadTickets();
+		this.start();
+		this.saveTickets();
+	} // end constructor
+	
+	public String menu(){
+		Scanner input = new Scanner(System.in);
+		System.out.println();
+		System.out.println("Help Desk Menu");
+		System.out.println();
+		System.out.println("0) Exit");
+		System.out.println("1) Login as admin");
+		System.out.println("2) Login as user");
+		System.out.println();
+		System.out.print("Action (0-2): ");
+		String response = input.nextLine();
+
+		return response;
+	} // end menu
+	
+	public void start(){
+		boolean keepGoing = true;
+		while (keepGoing){
+			String response = menu();
+			if (response.equals("0")){
+				keepGoing = false;
+			} else if (response.equals("1")){
+				System.out.println("Admin login");
+				if (this.admin.login()){
+					startAdmin();
+				} // end if
+			} else if (response.equals("2")){
+				System.out.println("User login");
+				this.loginAsUser();
+			} else {
+				System.out.println("Invalid Action. Please enter 0, 1, or 2");
+			} // end if
+		} // end while
+	} // end start
+
+} // end TicketManager class
