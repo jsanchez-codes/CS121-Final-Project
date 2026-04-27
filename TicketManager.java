87342e590ee3b3f62d7b0e5c569ffe6b2855a8ff
jsanchez-codes <julian.sanchez@bsu.edu>
2026-04-27 23:19:15 +0000

    Updated Ticket Manager

--- TicketManager.java
+++ TicketManager.java
@@ -5,7 +5,7 @@ public class TicketManager implements HelpDesk {
 
 	Admin admin = new Admin();
 	TicketList tickets = new TicketList();
-	ArrayList<String> categories = new ArrayList<>(List.of("Software","Hardware","Account"));	
+	ArrayList<String> categories = new ArrayList<>(List.of("Hardware","Software","Account","Network","Security"));	
 
 	public static void main(String[] args){
 		new TicketManager();
@@ -23,8 +23,8 @@ public class TicketManager implements HelpDesk {
 		System.out.println("Help Desk Menu");
 		System.out.println();
 		System.out.println("0) Exit");
-		System.out.println("1) Login as admin");
-		System.out.println("2) Login as user");
+		System.out.println("1) Create a ticket");
+		System.out.println("2) Login as admin");
 		System.out.println();
 		System.out.print("Action (0-2): ");
 		String response = input.nextLine();
@@ -43,7 +43,17 @@ public class TicketManager implements HelpDesk {
 				String description;
 
 				System.out.println("What category is your technology issue: ");
-				for(int = 0; i < categories.length();i++){
+				for(int i = 0; i < categories.length();i++){
+					System.out.printf("%d) %s\n",i,categories.get(i));
+				}
+				System.out.println("What category is your issue(enter number above): ");
+				categoryIndex = scnr.nextInt();
+
+				System.out.print("Please enter a description of your issue: ");
+				description = scnr.nextLine();
+
+				tickets.add(new Ticket(categories.get(categoryIndex),description));
+			} else if (response.equals("2"){
 				System.out.println("Admin login");
 				if (this.admin.login()){
 					startAdmin();
