13f5cf15ee10487910722d4dd824d89002b116d2
jsanchez-codes <julian.sanchez@bsu.edu>
2026-04-28 00:43:58 +0000

    Updated Admin

--- Admin.java
+++ Admin.java
@@ -20,8 +20,8 @@ public class Admin extends User implements HelpDesk, Serializable {
 		System.out.println("Admin Menu");
 		System.out.println();
 		System.out.println("0) Sign Out");
-		System.out.println("1) Add new user");
-		System.out.println("2) Delete user");
+		System.out.println("1) View Tickets");
+		System.out.println("2) Resolve Tickets");
 		System.out.println();
 		System.out.print("Action (0-2): ");
 
